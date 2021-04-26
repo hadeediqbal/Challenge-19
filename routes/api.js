c927b345a973eec933008c4c5a213e39f056ebf0
hadeediqbal <hadeediqbal@yahoo.com>
2021-04-25 23:30:59 -0500

    added api routing

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+var path = require("path");
+
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// SINGLE POST
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+// BULK POST
+router.post("/api/transaction/bulk", ({body}, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+// SEND ALL TRANSACTIONS BY DECENDING ORDER
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
